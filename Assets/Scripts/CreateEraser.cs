115b371e0302043f4bb4952278f710bcfa9ca13d
ning-hu <ning.hu1205@gmail.com>
2018-02-12 18:21:34 -0800

    added some enemy scripts

---
+++ Assets/Scripts/CreateEraser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateEraser : MonoBehaviour {
+
+	private float InstantiationTimer = 1f;
+//	GameObject eraser = GameObject.FindWithTag("Eraser");
+	public GameObject eraser;
+
+	void Update () {
+		CreatePrefab();
+	}
+
+	void CreatePrefab()
+	{
+		InstantiationTimer -= Time.deltaTime;
+		if (InstantiationTimer <= 0)
+		{
+			GameObject eraserCopy = Instantiate(eraser, transform.position, Quaternion.identity) as GameObject;
+			eraserCopy.SetActive (true);
+			InstantiationTimer = 1f;
+		}
+	}
+}

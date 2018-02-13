45ed3b67f062d2e60d98e531cbfe5b72a2af8a55
kyrnen <angelakam.lam@gmail.com>
2018-02-12 20:23:59 -0800

    updated Camera Controller
    not finished

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,16 +8,21 @@ public class CameraController : MonoBehaviour {
 
     private Vector2 offset;
     private Vector2 playerPos;
+    private Vector2 playerScreenPosition;
+    private Vector2 screenSize;
+
     
 	// Use this for initialization
 	void Start ()
     {
         playerPos = player.transform.position;
         offset = transform.position - player.transform.position;
+        screenSize = new Vector2(Screen.width, Screen.height);
+        playerScreenPosition = Camera.main.ScreenToWorldPoint(playerPos);
 	}
-	
-	// LateUpdate is called after Update each frame
-	void LateUpdate ()
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate ()
     {
         transform.position = playerPos + offset;
 	}

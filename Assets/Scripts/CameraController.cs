ece7c3b02c2396c56d9b7454472bddad442d4d9c
kyrnen <angelakam.lam@gmail.com>
2018-02-26 18:29:25 -0800

    Camera Modification
    Reverted the camera. I'll make it prettier i promise

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,26 +5,42 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     public GameObject player;
-
     private Vector2 offset;
     private Vector2 playerPos;
     private Vector2 playerScreenPosition;
     private Vector2 screenSize;
 
-    
-	// Use this for initialization
-	void Start ()
+    public float dampTime = 0.15f;
+    private Vector3 velocity = Vector3.zero;
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
     {
-        playerPos = player.transform.position;
-        offset = transform.position - player.transform.position;
-        screenSize = new Vector2(Screen.width, Screen.height);
-        playerScreenPosition = Camera.main.ScreenToWorldPoint(playerPos);
-	}
+        if (target)
+        {
+            Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
+            Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+            Vector3 destination = transform.position + delta;
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+        }
+
+    }
+}
+
+
+// Use this for initialization
+//void Start ()
+  //  {
+    //    playerPos = player.transform.position;
+      //  offset = transform.position - player.transform.position;
+        //screenSize = new Vector2(Screen.width, Screen.height);
+      //  playerScreenPosition = Camera.main.ScreenToWorldPoint(playerPos);
+	//}
 
     // LateUpdate is called after Update each frame
-    void LateUpdate ()
-    {
-        transform.position = playerPos + offset;
-	}
+    //void LateUpdate ()
+    //{
+        //transform.position = playerPos + offset;
+	//}
 
-}

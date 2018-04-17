fd829a7f0727a26eddb7e9a674cde118491ebd08
Chanel Young <chanelyoung99@gmail.com>
2018-04-16 19:12:11 -0700

    i want to pull

---
+++ Assets/Scripts/BossGuy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossGuy : MonoBehaviour {
+
+	public GameObject projectile; 
+	// Use this for initialization
+	void Start () {
+		ShootThreeProjectiles (); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void ShootThreeProjectiles(){
+		Instantiate (projectile, new Vector2(gameObject.transform.position.x, gameObject.transform.position.y), Quaternion.identity);
+		Instantiate (projectile, new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + 1), Quaternion.identity);
+		Instantiate (projectile, new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 1), Quaternion.identity);
+
+	}
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+	public bool facingLeft; 
+	public int speed; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.right * speed * Time.deltaTime);
+	}
+}

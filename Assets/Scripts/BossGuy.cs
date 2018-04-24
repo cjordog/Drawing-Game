798de4fd04096526caa299ee5d7591a700364c47
Chanel Young <chanelyoung99@gmail.com>
2018-04-23 20:59:14 -0700

    made boss with reflectable projectiles

--- Assets/Scripts/BossGuy.cs
+++ Assets/Scripts/BossGuy.cs
@@ -1,78 +1,94 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class BossGuy : MonoBehaviour {
 
-	public GameObject projectile;
-	public int chargespeed = 12; 
+	public Slider BossHealthSlider;
 
+	public float BossHealth; 
+
+	private int left = 1; 
+	private int right = 2; 
+	private int notmoving = 3; 
+
+	public int chargespeed = 12; 
+	public int currentChargingDirection; 
+
+	public GameObject projectile;
+	public GameObject reflectableProjectile; 
 	[SerializeField]
 	private GameObject HealthDisplay;
 	private Health UIHealthScript;
-	public GameObject player;
+	public Transform player;
 
-	public bool isCharging = true;
-	private bool currentChargingDirection = true; 
+	private bool isCharging = false; 
+	private bool isShooting = false; 
 	// Use this for initialization
 	void Start () {
+		BossHealth = 1.0f; 
 		UIHealthScript = HealthDisplay.GetComponent<Health>();
 		ShootThreeProjectiles ();  
-		isCharging = true; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		BossHealthSlider.value = BossHealth; 
 		PossiblyAttack (); 
 		if (isCharging) {
-			Charge (currentChargingDirection);
+			Charge ();
 		}
 	}
-
+	public void getInjured(){
+		BossHealth -= .1f; 
+	}
 	void PossiblyAttack(){
-		int randomInt = Random.Range (0, 80);
-		if (randomInt == 0 && !isCharging) {
+		int randomInt = Random.Range (0, 60);
+		if (randomInt == 0 && !isCharging && !isShooting) {
 			isCharging = true; 
-			if (player.transform.position.x < transform.position.x)
-				currentChargingDirection = true;
+			if (player.transform.position.x <= transform.position.x)
+				currentChargingDirection = left;
 			else
-				currentChargingDirection = false; 
+				currentChargingDirection = right; 
 
-		} else if(randomInt == 1) {
+		} else if(randomInt == 1 && !isCharging && !isShooting) {
 			ShootThreeProjectiles (); 
 		}
 	}
 
-	void Charge(bool left){
-		if (left) {
-			if (transform.position.x <= player.transform.position.x - 2) {
-				Debug.Log ("I'm out of range and should be stopping");
+	void Charge(){
+		if (currentChargingDirection == left) {
+			if (transform.position.x <= player.transform.position.x - 5) {
 				isCharging = false; 
+				currentChargingDirection = notmoving; 
+			} else {
+				transform.Translate (Vector3.left * chargespeed * Time.deltaTime);
 			}
-			transform.Translate (Vector3.left * chargespeed * Time.deltaTime);
 		}
-		else {
-			if (transform.position.x >= player.transform.position.x + 2) {
+		else if(currentChargingDirection == right){
+			if (transform.position.x >= player.transform.position.x + 5) {
 				Debug.Log ("I'm out of range and should be stopping");
 				isCharging = false; 
+				currentChargingDirection = notmoving; 
+			} else {
+				transform.Translate (Vector3.right * chargespeed * Time.deltaTime);
 			}
-			transform.Translate (Vector3.right * chargespeed * Time.deltaTime);
 		}
 	}
 
 	void ShootThreeProjectiles(){
 		StartCoroutine (ShootThreeProjectilesCoroutine());
 
-
 	}
 	IEnumerator ShootThreeProjectilesCoroutine()
 	{
+		isShooting = true; 
 		Instantiate (projectile, new Vector2(gameObject.transform.position.x, gameObject.transform.position.y +1), Quaternion.identity);
 		yield return new WaitForSeconds (.4f);
-		Instantiate (projectile, new Vector2(gameObject.transform.position.x, gameObject.transform.position.y), Quaternion.identity);
+		Instantiate (reflectableProjectile, new Vector2(gameObject.transform.position.x, gameObject.transform.position.y), Quaternion.identity);
 		yield return new WaitForSeconds (.4f); 
 		Instantiate (projectile, new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 1), Quaternion.identity);
-
+		isShooting = false; 
 	}
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,30 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour {
-	public bool facingRight = true; 
 	public int speed = 5; 
-	public GameObject player; 
+	public Transform player; 
 	public GameObject LineComponent; 
-
+	public bool isReflectingProjectile; 
+	public bool reflected = false; 
 	[SerializeField]
 	private GameObject HealthDisplay;
 	private Health UIHealthScript;
 
-	private int facingRightInt = 0; 
+	private bool facingRight = true; 
 	// Use this for initialization
 	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform; 
 		HealthDisplay = GameObject.Find ("HealthDisplay"); 
 		UIHealthScript = HealthDisplay.GetComponent<Health>();
-
 		if (player.transform.position.x < transform.position.x)
-			facingRightInt = 0;
+			facingRight = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (transform.position.x >= player.transform.position.x + 10 || transform.position.x <= player.transform.position.x - 10)
+		if ((transform.position.x >= player.transform.position.x + 15 && facingRight) || (transform.position.x <= player.transform.position.x - 15 && !facingRight))
 			Object.Destroy (this.gameObject);
-		if(facingRightInt == 1)
+		if(facingRight == true)
 			transform.Translate (Vector3.right * speed * Time.deltaTime);
 		else
 			transform.Translate (Vector3.left * speed * Time.deltaTime);
@@ -34,9 +34,22 @@ public class Projectile : MonoBehaviour {
 
 
 	void OnCollisionEnter2D(Collision2D col){
+		LineScript script = col.gameObject.GetComponent<LineScript> (); 
+		if (script != null && !reflected) { //if it hit a line component object
+			facingRight = !facingRight; 
+			Debug.Log ("reflected");
+			reflected = true; 
+		}
 		if (col.gameObject.tag == "Player") {
 			UIHealthScript.loseHealth ();  
+			Destroy (gameObject);
+		} else if (col.gameObject.tag == "Enemy" && reflected) {
+			Debug.Log ("Hit Boss when reflected");
+			col.gameObject.GetComponent <BossGuy> ().getInjured (); 
+			Destroy (gameObject); 
+		} else if (!isReflectingProjectile) {
+			Destroy (gameObject); 
 		}
-		Destroy (gameObject); 
+		 
 	}
 }

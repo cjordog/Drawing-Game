091a16868147f7a0e3e149a61f6c9149ca2aa159
KevinChuang2 <kevinchuang2@gmail.com>
2018-06-04 13:48:28 -0700

    game over screen

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -8,4 +8,8 @@ public class Menu : MonoBehaviour {
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void replay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
 }
